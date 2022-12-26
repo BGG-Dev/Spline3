39cd3c6c81fc043d0bcf84b32fd1b4b55f2d8a1f
Bohdan Pylypchenko <bogdan314ylypchenko@gmail.com>
2022-12-26 21:37:43 +0200

    Finishing gui

--- Spline3Lib/SplineInterpolator.cs
+++ Spline3Lib/SplineInterpolator.cs
@@ -8,98 +8,106 @@ namespace Spline3Lib
      */
     public class SplineInterpolator
     {
-        private double[] hX;
+        private int N;
 
-        private double[] hY;
+        private double[] hx;
 
-        private Tuple<double, double>[] nodes;
+        private double[] hy;
 
-        private double[] b;
-
-        private double[] c;
-
-        private double[] d;
-
-        private int N { get { return nodes.Length - 1; } }
-        
-        /*
-         * Default constructor
-         * nodes - tuple <vC, f(vC)> to run interpolation on
-         */
-        public SplineInterpolator(Tuple<double, double>[] nodes)
+        public Func<double, double> Interpolate(Tuple<double, double>[] nodes)
         {
-            // Initializing nodes
-            this.nodes = nodes;
+            // Define N as spline count. Spline count = node count - 1
+            N = nodes.Length - 1;
 
-            // Initializing empty coefficients
-            b = new double[N];
-            d = new double[N];
+            // Creating arrays
+            hx         = new double[N];
+            hy         = new double[N];
+            double[] b = new double[N];
+            double[] c;
+            double[] d = new double[N];
 
-            // Calculating hX and hY
-            hX = new double[N];
-            hY = new double[N];
-            for (int i = 0; i < N; i++)
+            // Initializing hx
+            for (int i = 0; i < N; i++) // [0; n - 1] => [1; n]
             {
-                hX[i] = nodes[i + 1].Item1 - nodes[i].Item1;
-                hY[i] = nodes[i + 1].Item2 - nodes[i].Item2;
+                hx[i] = nodes[i + 1].Item1 - nodes[i].Item1;
+                hy[i] = nodes[i + 1].Item2 - nodes[i].Item2;
             }
-        }
 
-        /*
-         * Runs interpolation
-         */
-        public Spline[] Interpolate()
-        {
-            // Calculating C
+            // Calculating c
             c = CalculateC();
 
-            // Calculating B and D
-            for (int i = 0; i < N - 1; i++)
+            // Calculating b and d
+            for (int i = 0; i < N - 1; i++) // [0; n - 2] => [1; n - 1]
             {
-                b[i] = ((hY[i] / hX[i]) - (2 * c[i] + c[i + 1]) * hX[i] / 3.0);
-                d[i] = (c[i + 1] - c[i]) / (3 * hX[i]);
+                b[i] = (hy[i] / hx[i]) - ((hx[i] / 3.0) * (c[i + 1] + 2 * c[i]));
+                d[i] = (c[i + 1] - c[i]) / (3.0 * hx[i]);
             }
-            b[N - 1] = (hY[N - 1] / hX[N - 1]) - ((2.0/3.0) * hX[N - 1] * c[N - 1]);
-            d[N - 1] = -1.0 * (c[N - 1] / (3.0 * hX[N - 1]));
+            b[N - 1] = (hy[N - 1] / hx[N - 1]) - ((2.0 / 3.0) * hx[N - 1] * c[N - 1]);
+            d[N - 1] = -1.0 * (c[N - 1] / (3.0 * hx[N - 1]));
 
-            // Creating splines
+            // Creating spline array
             Spline[] splines = new Spline[N];
             for (int i = 0; i < N; i++)
             {
                 splines[i] = new Spline(nodes[i].Item1, nodes[i].Item2, b[i], c[i], d[i]);
             }
 
-            // Returning
-            return splines;
+            // Return spline function;
+            return x =>
+            {
+                if (x < nodes[0].Item1)
+                {
+                    return splines[0].F(x);
+                }
+
+                for (int i = 0; i < nodes.Length - 1; i++)
+                {
+                    if (nodes[i].Item1 <= x && x <= nodes[i + 1].Item1)
+                    {
+                        return splines[i].F(x);
+                    }
+                }
+
+                return splines[splines.Length - 1].F(x);
+            };
         }
 
         private double[] CalculateC()
         {
-            // Calculating C system coefficients
-            double[,] matrixCoefficients = new double[N, N];
-            matrixCoefficients[0, 0] = 1;
-            matrixCoefficients[N - 1, N - 1] = 1;
-            for (int i = 1; i < N - 1; i++)
+            // Allocating matrix
+            double[,] matrixCoefficients = new double[N + 1, N + 1];
+
+            // Fake coefficients
+            matrixCoefficients[0, 0] = 1; // [0, 0] => [1, 1]
+            matrixCoefficients[N, N] = 1; // [n, n] => [n + 1, n + 1]
+
+            // Calculating rest
+            for (int i = 1; i < N; i++) // [1; n - 1] => [2; n]
             {
-                matrixCoefficients[i, i - 1] = hX[i];
-                matrixCoefficients[i, i]     = 2.0 * (hX[i] + hX[i + 1]);
-                matrixCoefficients[i, i + 1] = hX[i + 1];
+                matrixCoefficients[i, i - 1] = hx[i - 1];
+                matrixCoefficients[i, i] = 2.0 * (hx[i - 1] + hx[i]);
+                matrixCoefficients[i, i + 1] = hx[i];
             }
 
-            // Calculating freeCoefficients members of system
-            double[] freeCoefficients = new double[N];
-            for (int i = 1; i < N - 1; i++)
+            // Calculating free coefficients
+            double[] freeCoefficients = new double[N + 1];
+            for (int i = 1; i < N; i++) // [1; n - 1] => [2; n]
             {
-                freeCoefficients[i] = 3.0 * ((hY[i] / hX[i]) - ((hY[i - 1]) / hX[i - 1]));
+                freeCoefficients[i] = 3.0 * ((hy[i] / hx[i]) - (hy[i - 1] / hx[i - 1]));
             }
 
             // Solving system
-            var mA = Matrix<double>.Build.DenseOfArray(matrixCoefficients);
-            var vB = Vector<double>.Build.DenseOfArray(freeCoefficients);
-            var vC = mA.Solve(vB);
+            var A = Matrix<double>.Build.DenseOfArray(matrixCoefficients);
+            var B = Vector<double>.Build.DenseOfArray(freeCoefficients);
+            var C = A.Solve(B);
 
             // Returning
-            return vC.ToArray();
+            double[] result = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                result[i] = C.At(i);
+            }
+            return result;
         }
     }
 }
